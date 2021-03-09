c90e110d53a4c4bf38920189a467656e3a5d5e15
Vasiliy <chaylord@bk.ru>
2021-03-09 19:58:41 +0500

    Добавленна модель GetItems

---
+++ DBSQLONE/Models/Tables/GetItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBSQLONE.Models.Tables
+{
+    public class GetItemsGetItems
+    {
+        public int Id { get; set; }
+        public int IdItem { get; set; }
+        public int IdPlayer { get; set; }
+        public DateTime Received { get; set; }
+    }
+}
